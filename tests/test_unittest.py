4b30b3e16fa826fe939d6c63a8a8721e16122b17
colin.frame <colin.frame@soprasteria.com>
2023-10-24 08:33:52 +0100

    Cosmetic items (adding/removing blank lines). Add .vscode to .ignore

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,6 +1,7 @@
 import inc_dec    # The code to test
 import unittest   # The test framework
 
+
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
         self.assertEqual(inc_dec.increment(3), 4)
@@ -9,6 +10,6 @@ class Test_TestIncrementDecrement(unittest.TestCase):
     def test_decrement(self):
         self.assertEqual(inc_dec.decrement(3), 4)
 
+
 if __name__ == '__main__':
     unittest.main()
-  
